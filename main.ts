35a5fe3ad8459eb5243812fef8d19b11cdb8d639
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-09-08 11:30:21 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,54 @@
+/**
+ * Forças Físicas - Aceleração
+ */
+function desorientado () {
+    basic.showLeds(`
+        # . . # .
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # . # #
+        # . . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # # . # #
+        . # . . #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # . . #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+}
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) < 500 && input.acceleration(Dimension.X) > -500) {
+        basic.showIcon(IconNames.Happy)
+    }
+    if (input.acceleration(Dimension.X) > 500 || input.acceleration(Dimension.X) < -500) {
+        if (input.acceleration(Dimension.Y) > 500 || input.acceleration(Dimension.Y) < -500) {
+            for (let index = 0; index < 2; index++) {
+                desorientado()
+            }
+            basic.showLeds(`
+                # # . # #
+                # # . # #
+                . . . . .
+                . # # # .
+                # . . . #
+                `)
+            basic.pause(500)
+        }
+    }
+})
