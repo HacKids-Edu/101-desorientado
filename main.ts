3e0310da15cd7cdf681f096d32c4113557888d87
HacKids Education <65713501+HacKids-Edu@users.noreply.github.com>
2020-09-20 10:46:45 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
-/**
- * Forças Físicas - Aceleração
- */
-function desorientado () {
+function desorientadoEsquerda () {
     basic.showLeds(`
         # . . # .
         # # . # #
@@ -31,24 +28,65 @@ function desorientado () {
         # . . . #
         `)
 }
+function desorientadoDireita () {
+    basic.showLeds(`
+        # . . # .
+        # # . # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . #
+        # # . # #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        . # . . #
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # . # #
+        # . . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+}
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (input.acceleration(Dimension.X) < 500 && input.acceleration(Dimension.X) > -500) {
         basic.showIcon(IconNames.Happy)
     }
-    if (input.acceleration(Dimension.X) > 500 || input.acceleration(Dimension.X) < -500) {
-        if (input.acceleration(Dimension.Y) > 500 || input.acceleration(Dimension.Y) < -500) {
-            for (let index = 0; index < 2; index++) {
-                desorientado()
-            }
-            basic.showLeds(`
-                # # . # #
-                # # . # #
-                . . . . .
-                . # # # .
-                # . . . #
-                `)
-            basic.pause(500)
+    if (input.acceleration(Dimension.X) > 500) {
+        for (let index = 0; index < 2; index++) {
+            desorientadoDireita()
         }
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(500)
+    }
+    if (input.acceleration(Dimension.X) < -500) {
+        for (let index = 0; index < 2; index++) {
+            desorientadoEsquerda()
+        }
+        basic.showLeds(`
+            # # . # #
+            # # . # #
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        basic.pause(500)
     }
 })
